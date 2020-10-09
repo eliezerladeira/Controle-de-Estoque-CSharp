069cfa9e0a294a8028e2b1384d811f6d55373835
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-08 23:58:49 -0300

    Criação das classes ModeloCategoria e DadosDaConexao. A classe ModeloCategoria irá representar a tabela de categoria e a classe DadosDaConexão irá representar os dados da conexão com o banco de dados, como por exemplo: servidor, login, senha e nome do banco.

---
+++ Estoque/Controle de Estoque/DAL/DadosDaConexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexao
+    {
+        public static String StringDeConexao
+        {
+            get
+            {
+                return "Data Source=DESKTOP-7D46FRE\\SQLEXPRESS;Initial Catalog=Estoque;User ID=sa;Password=123456";
+            }
+        }
+    }
+}
---
+++ Estoque/Controle de Estoque/Modelo/ModeloCategoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ModeloCategoria
+    {
+        private int cat_cod;
+        private String cat_nome;
+
+        public int CatCod
+        {
+            get { return this.cat_cod; }
+            set { this.cat_cod = value; }
+        }
+
+        public String CatNome
+        {
+            get { return this.cat_nome; }
+            set { this.cat_nome = value; }
+        }
+    }
+}
