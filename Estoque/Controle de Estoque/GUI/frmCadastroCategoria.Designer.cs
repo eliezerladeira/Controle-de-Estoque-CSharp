22cd40ef73d479e8650b986c20157b0fc9d268a1
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-10 14:29:07 -0300

    Formulário Cadastro de Categorias
    Implementação de um modelo de formulário que será utilizado em todas as telas de cadastro do sistema.
    
    Implementação do formulário de cadastro de categorias. Esse formulário utiliza como classe pai a classe do formulário modelo.
    
    O fomulário de cadastro de categoria irá cadastrar, excluir, localizar e alterar uma categoria.

--- Estoque/Controle de Estoque/GUI/frmCadastroCategoria.Designer.cs
+++ Estoque/Controle de Estoque/GUI/frmCadastroCategoria.Designer.cs
@@ -28,12 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.panDados.SuspendLayout();
             this.panBotoes.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panDados
+            // 
+            this.panDados.Controls.Add(this.txtCategoria);
+            this.panDados.Controls.Add(this.txtCodigo);
+            this.panDados.Controls.Add(this.lblCategoria);
+            this.panDados.Controls.Add(this.lblCodigo);
             // 
             // btnCancelar
             // 
             this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnLocalizar
             // 
@@ -42,6 +55,7 @@
             // btnSalvar
             // 
             this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // btnExcluir
             // 
@@ -54,6 +68,39 @@
             // btnInserir
             // 
             this.btnInserir.FlatAppearance.BorderSize = 0;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(18, 31);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(18, 85);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 1;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(21, 47);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(21, 101);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(376, 20);
+            this.txtCategoria.TabIndex = 3;
             // 
             // frmCadastroCategoria
             // 
@@ -62,11 +109,19 @@
             this.Name = "frmCadastroCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Categorias";
+            this.Load += new System.EventHandler(this.frmCadastroCategoria_Load);
+            this.panDados.ResumeLayout(false);
+            this.panDados.PerformLayout();
             this.panBotoes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblCodigo;
     }
 }
--- Estoque/Controle de Estoque/GUI/frmCadastroCategoria.cs
+++ Estoque/Controle de Estoque/GUI/frmCadastroCategoria.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BLL;
+using DAL;
+using Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +16,59 @@ namespace GUI
         public frmCadastroCategoria()
         {
             InitializeComponent();
+        }
+
+        public void LimpaTela()
+        {
+            txtCodigo.Clear();
+            txtCategoria.Clear();
+        }
+
+        private void frmCadastroCategoria_Load(object sender, EventArgs e)
+        {
+            this.alteraBotoes(1);
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            this.operacao = "Inserir";
+            this.alteraBotoes(2);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.LimpaTela();
+            this.alteraBotoes(1);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ModeloCategoria modelo = new ModeloCategoria();
+                modelo.CatNome = txtCategoria.Text;
+
+                DALConexao cx = new DALConexao(DadosDaConexao.StringDeConexao);
+                BLLCategoria bll = new BLLCategoria(cx);
+
+                if (this.operacao == "Inserir")
+                {
+                    bll.Incluir(modelo);
+                    MessageBox.Show("Categoria " + txtCategoria.Text + " cadastrada com sucesso! Código: " + modelo.CatCod.ToString());
+                }
+                else
+                {
+                    modelo.CatCod = Convert.ToInt32(txtCodigo.Text);
+                    bll.Alterar(modelo);
+                    MessageBox.Show("Categoria " + txtCategoria.Text + " alterada com sucesso!");
+                }
+                this.LimpaTela();
+                this.alteraBotoes(1);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
         }
     }
 }
