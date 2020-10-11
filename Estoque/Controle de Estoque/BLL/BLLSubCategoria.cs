3e6404392fe5b9877ef4bb56433e3638b014e383
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-11 00:13:33 -0300

    BLLSubCategoria
    Implementação da classe BLLSubCategoria para tratar as regras de negócio relacionadas com as SubCategorias.

---
+++ Estoque/Controle de Estoque/BLL/BLLSubCategoria.cs
@@ -0,0 +1,66 @@
+﻿using DAL;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLSubCategoria
+    {
+        private DALConexao conexao;
+
+        public BLLSubCategoria(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloSubCategoria modelo)
+        {
+            if (modelo.ScatNome.Trim().Length == 0)
+                throw new Exception("A descrição da subcategoria é obrigatória!");
+
+            if (modelo.CatCod <= 0)
+                throw new Exception("A categoria é obrigatória!");
+
+            DALSubCategoria DALobj = new DALSubCategoria(conexao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Alterar(ModeloSubCategoria modelo)
+        {
+            if (modelo.ScatNome.Trim().Length == 0)
+                throw new Exception("A descrição da subcategoria é obrigatória!");
+
+            if (modelo.CatCod <= 0)
+                throw new Exception("A categoria é obrigatória!");
+
+            if (modelo.ScatCod <= 0)
+                throw new Exception("A subcategoria é obrigatória!");
+
+            DALSubCategoria DALobj = new DALSubCategoria(conexao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Excluir(int codigo)
+        {
+            DALSubCategoria DALobj = new DALSubCategoria(conexao);
+            DALobj.Excluir(codigo);
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DALSubCategoria DALobj = new DALSubCategoria(conexao);
+            return DALobj.Localizar(valor);
+        }
+
+        public ModeloSubCategoria CarregaModeloSubCategoria(int codigo)
+        {
+            DALSubCategoria DALobj = new DALSubCategoria(conexao);
+            return DALobj.CarregaModeloSubCategoria(codigo);
+        }
+    }
+}
