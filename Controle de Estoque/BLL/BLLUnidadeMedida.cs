4b64b930b1e3b42d7eafaf8008cd82d1f95b08f1
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-11-02 16:59:45 -0300

    Implementando a classe BLLUnidadeDeMedida

---
+++ Controle de Estoque/BLL/BLLUnidadeMedida.cs
@@ -0,0 +1,60 @@
+﻿using DAL;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLUnidadeMedida
+    {
+        private DALConexao conexao;
+
+        public BLLUnidadeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloUnidadeMedida modelo)
+        {
+            if (modelo.UmedNome.Trim().Length == 0)
+                throw new Exception("A descrição da unidade de medida é obrigatória!");
+
+            DALUnidadeMedida DALobj = new DALUnidadeMedida(conexao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Alterar(ModeloUnidadeMedida modelo)
+        {
+            if (modelo.UmedNome.Trim().Length == 0)
+                throw new Exception("A descrição da unidade de medida é obrigatória!");
+
+            if (modelo.UmedCod <= 0)
+                throw new Exception("A unidade de medida é obrigatória!");
+
+            DALUnidadeMedida DALobj = new DALUnidadeMedida(conexao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Excluir(int codigo)
+        {
+            DALUnidadeMedida DALobj = new DALUnidadeMedida(conexao);
+            DALobj.Excluir(codigo);
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DALUnidadeMedida DALobj = new DALUnidadeMedida(conexao);
+            return DALobj.Localizar(valor);
+        }
+
+        public ModeloUnidadeMedida CarregaModeloUnidadeMedida(int codigo)
+        {
+            DALUnidadeMedida DALobj = new DALUnidadeMedida(conexao);
+            return DALobj.CarregaModeloUnidadeMedida(codigo);
+        }
+    }
+}
