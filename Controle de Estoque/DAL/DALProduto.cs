247d74c1545c889cbe835458b1ce55c8538e3eb7
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-04-30 22:26:17 -0300

    Classe DALProduto
    Implementação da classe DALProduto.

---
+++ Controle de Estoque/DAL/DALProduto.cs
@@ -0,0 +1,103 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALProduto
+    {
+        private DALConexao conexao;
+
+        public DALProduto(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloProduto modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "INSERT INTO produto(pro_nome, pro_descricao, pro_foto, pro_valorpago, pro_valorvenda, pro_qtde, undmed_cod, cat_cod, scat_cod) " +
+                "VALUES (@nome, @descricao, @foto, @valorpago, @valorvenda, @qtde, @undmedcod, @catcod, @scatcod); SELECT @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@nome", modelo.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", modelo.ProDescricao);
+            cmd.Parameters.AddWithValue("@foto", System.Data.SqlDbType.Image);
+
+            if (modelo.ProFoto == null)
+            {
+                cmd.Parameters["@foto"].Value = DBNull.Value;
+            }
+            else
+            {
+                cmd.Parameters["@foto"].Value = modelo.ProFoto;
+            }
+
+            cmd.Parameters.AddWithValue("@valorpago", modelo.ProValorPago);
+            cmd.Parameters.AddWithValue("@valorvenda", modelo.ProValorVenda);
+            cmd.Parameters.AddWithValue("@qtde", modelo.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", modelo.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", modelo.CatCod);
+            cmd.Parameters.AddWithValue("@scatco", modelo.SCatCod);
+            conexao.Conectar();
+            modelo.ProCod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+        }
+/*
+        public void Alterar(ModeloCategoria modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "UPDATE categoria SET cat_nome = @nome WHERE cat_cod = @codigo";
+            cmd.Parameters.AddWithValue("@nome", modelo.CatNome);
+            cmd.Parameters.AddWithValue("@codigo", modelo.CatCod);
+            conexao.Conectar();
+            // executenonquery: nenhuma informação do banco
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        public void Excluir(int codigo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "DELETE FROM categoria WHERE cat_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM categoria WHERE cat_nome LIKE '%" + valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloCategoria CarregaModeloCategoria(int codigo)
+        {
+            ModeloCategoria modelo = new ModeloCategoria();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "SELECT * FROM categoria WHERE cat_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            // executereader: várias informações/registros do banco de dados
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.CatCod = Convert.ToInt32(registro["cat_cod"]);
+                modelo.CatNome = Convert.ToString(registro["cat_nome"]);
+            }
+            conexao.Desconectar();
+            return modelo;
+        }
+*/
+    }
+}
